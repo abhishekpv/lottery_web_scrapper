3ea752a58b8b2cfc719f167fdcb5b980b227e681
Abhishek P V <abhishek.pottammal@qburst.com>
2024-07-04 17:25:39 +0530

    vercel-hosting

---
+++ api/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const scrapeLotteryWinners = require("../app/scrapeLotteryWinners");
+const findWinner = require("../app/findWinner");
+
+const app = express();
+app.use(express.json());
+const router = express.Router();
+app.use("/", router);
+
+router.get("/lotteries", async (req, res, next) => {
+  const lotteries = await scrapeLotteryWinners();
+  res.status(200).json(lotteries);
+});
+
+router.get("/winner", async (req, res, next) => {
+  const { lottery, pdf } = req.body;
+  const winner = await findWinner(lottery, pdf);
+  res.status(200).json(winner);
+});
+
+module.exports = app;
