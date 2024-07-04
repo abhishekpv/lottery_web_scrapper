40e5132f9885bf647a1d7fe27576e9c078e7157e
Abhishek P V <abhishek.pottammal@qburst.com>
2024-07-04 17:53:50 +0530

    fix api bug

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const scrapeLotteryWinners = require("./app/scrapeLotteryWinners");
 const findWinner = require("./app/findWinner");
+const { injectSpeedInsights } = require("@vercel/speed-insights");
 
 const app = express();
 app.use(express.json());
 const router = express.Router();
+injectSpeedInsights();
+
 app.use("/", router);
 
 router.get("/lotteries", async (req, res, next) => {
@@ -13,7 +16,8 @@ router.get("/lotteries", async (req, res, next) => {
 });
 
 router.get("/winner", async (req, res, next) => {
-  const { lottery, pdf } = req.body;
+  console.log("hi", req.query);
+  const { lottery, pdf } = req.query;
   const winner = await findWinner(lottery, pdf);
   res.status(200).json(winner);
 });
